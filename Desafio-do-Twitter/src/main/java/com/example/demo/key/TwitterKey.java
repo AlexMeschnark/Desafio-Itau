ba64f43bf2413f8fdb8c9e096f3b355c15daa1bb
Alex Frederico Meschnark <alex.meschnark@hotmail.com>
2020-12-11 18:42:13 -0300

    Update TwitterKey.java

--- Desafio-do-Twitter/src/main/java/com/example/demo/key/TwitterKey.java
+++ Desafio-do-Twitter/src/main/java/com/example/demo/key/TwitterKey.java
@@ -48,10 +48,11 @@ public class TwitterKey implements Serializable {
 	 public Twitter getTwitterConfiguration() throws TwitterException{
 	        ConfigurationBuilder cb = new ConfigurationBuilder()
 	                .setDebugEnabled(true)
-	                .setOAuthConsumerKey("sSAqJJ3V8SH6pbXPxiuxjVHDU")
-	        		.setOAuthConsumerSecret("bCZrAExfLt0MKBqiBi2mmJZbtchofChaSChCsJh5MBEwQDLcuf")
-	        		.setOAuthAccessToken("3082605442-hyCFl01SwfIFhrzhJQi3l4Bmb2n3KpO2g7UjJ3G")
-	        		.setOAuthAccessTokenSecret("NKihYGG6md5YNax0YC9glrru2V49FLoWC6AFkQ6caafXv");
+	                .setDebugEnabled(true)
+                .setOAuthConsumerKey("*********")
+                .setOAuthConsumerSecret("*********")
+                .setOAuthAccessToken("*********")
+                .setOAuthAccessTokenSecret("*********");
 
 	        TwitterFactory twitterFactory = new TwitterFactory(cb.build());
 	        Twitter twitter = twitterFactory.getInstance();
